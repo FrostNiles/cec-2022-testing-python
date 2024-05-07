c61f15a5e6f1fbdc3bd6c757b7edc5e18215a0f6
FrostNiles <opravil.jiri@email.cz>
2024-05-07 10:07:37 +0200

    update combine back to full spread of functions

--- combine-dim-func.py
+++ combine-dim-func.py
@@ -17,14 +17,14 @@ if __name__ == '__main__':
     # Create a ThreadPoolExecutor
     with concurrent.futures.ThreadPoolExecutor() as executor:
         futures = []
-        for i in range(12, 13):
+        for i in range(1, 13):
             if i in skipped:
                 continue
             # Run the test in a separate thread
             futures.append(executor.submit(run_test, i))
 
         # Wait for all the tests to finish or timeout after 60 seconds
-        for future in concurrent.futures.as_completed(futures, timeout=180):
+        for future in concurrent.futures.as_completed(futures):
             try:
                 future.result()
             except concurrent.futures.TimeoutError:
