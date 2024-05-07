851d061f6f6948e9373699426b795f7e23205544
FrostNiles <opravil.jiri@email.cz>
2024-05-07 05:42:03 +0200

    change python testing code to paralel one

--- combine-dim-func.py
+++ combine-dim-func.py
@@ -1,20 +1,33 @@
-import runpy
-import re
-import sys
+import subprocess
+import multiprocessing
+import os
+import time
+import concurrent.futures
 
-skipped = {6,7,8,12}
-for i in range(6, 13):
-    if i in skipped:
-        continue
-    # CEC 2022 has 12 functions
+skipped = {}
+
+def run_test(i):
     for j in [10, 20]:
         for k in range(1, j+1):
-            args = {
-                'arg1': str(i),
-                'arg2': str(j),
-                'arg3': str(k)
-            }
-            
-            runpy.run_path('./run-tests.py', init_globals=args)
+            # Set the dimension as a command line argument for the C++ program
+            args = [str(i), str(j), str(k)]
+            subprocess.run(['python', './run-tests.py'] + args)
+
+if __name__ == '__main__':
+    # Create a ThreadPoolExecutor
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = []
+        for i in range(1, 13):
+            if i in skipped:
+                continue
+            # Run the test in a separate thread
+            futures.append(executor.submit(run_test, i))
+
+        # Wait for all the tests to finish or timeout after 60 seconds
+        for future in concurrent.futures.as_completed(futures, timeout=180):
+            try:
+                future.result()
+            except concurrent.futures.TimeoutError:
+                print("A test took too long and was cancelled.")
 
 
